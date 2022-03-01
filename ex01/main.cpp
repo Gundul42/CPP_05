c9256c38e0d6ec1d0f3d5050f0ba38c684e99c72
Gundul <sampah@mailbox.org>
2022-03-01 18:22:39 +0100

    last

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 12:27:33 by graja             #+#    #+#             */
-/*   Updated: 2022/02/26 18:18:08 by graja            ###   ########.fr       */
+/*   Updated: 2022/02/28 11:26:55 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ int	main(void)
 {
 		Bureaucrat	john("John", 11);
 		Bureaucrat	momo("Momo", 21);
-		Form			neu("Test", 12,1200);
+		Form			neu("Test", 12,120);
 		Form			cpy;
 
 		john.setGrade(100);
 		momo.setGrade(129);
+		cpy = neu;
 		std::cout << john << std::endl << momo << std::endl;
 		momo = john;
 		std::cout << john << std::endl << momo << std::endl;
@@ -30,5 +31,6 @@ int	main(void)
 		john.signForm(neu);
 		john.incGrade();
 		john.signForm(neu);
+		john.signForm(cpy);
 	return (0);
 }
