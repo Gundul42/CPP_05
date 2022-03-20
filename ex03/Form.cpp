24a9de3dd06f49bf2774888fdbd315ce66502411
Gundul <sampah@mailbox.org>
2022-03-20 13:46:52 +0100

    fix wrong try/catch block

--- ex03/Form.cpp
+++ ex03/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 11:04:06 by graja             #+#    #+#             */
-/*   Updated: 2022/03/04 12:04:39 by graja            ###   ########.fr       */
+/*   Updated: 2022/03/20 13:45:09 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,10 @@ Form::Form(void): _name("Default form"), _signed(false), _grade2sign(150), _grad
 Form::Form(std::string name, int sign, int exec): _name(name), _signed(false), _grade2sign(sign),
 		_grade2exec(exec)
 {
-	try
-	{
 		if ((this->getGrade2Sign() < 1) || (this->getGrade2Exec() < 1))
 			throw Form::GradeTooHighException();
 		else if ((this->getGrade2Sign() > 150) || (this->getGrade2Exec() > 150))
 			throw Form::GradeTooLowException();
-	}
-	catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-		std::cout << "Error while constructing object, exiting..." << std::endl;
-		exit (-1);
-	}
 }
 
 Form::Form(const Form & cpy): _name(cpy._name), _signed(cpy._signed), _grade2sign(cpy._grade2sign),
